1d3b2d89b810e1172ee1d911ff1a61bf43cfde2a
Rayan fernandes <>
2021-04-28 16:47:55 +0000

    fixed crash when .env file is missing

--- run.py
+++ run.py
@@ -1,15 +1,16 @@
 import os
 
-
 def runSetup():
   def alert(missing="API_ID , API_HASH"):
     print(f"\nCopy your {missing} and save it into Secrets(Environment variables) Sidebar!\n")
 
-  if os.environ["API_ID"] == "NA":
+  api_id = os.getenv("API_ID")
+  if api_id == None:
     alert()
     return
 
-  if os.environ["SESSION_STRING"] == "NA":
+  session_string = os.getenv("SESSION_STRING")
+  if session_string == None:
     os.system("python3 app/generate_session_string.py")
     alert(missing = "SESSION_STRING")
     return
